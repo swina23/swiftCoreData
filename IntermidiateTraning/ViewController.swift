0bf8298f310dca4c4f5e289bf66144b8cc0cf3b6
tomo.h <tomo.the.slyb@gmail.com>
2019-12-19 15:39:10 +0900

    Initial Commit

---
+++ IntermidiateTraning/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IntermidiateTraning
+//
+//  Created by tomo on 2019/12/19.
+//  Copyright © 2019 tomo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
