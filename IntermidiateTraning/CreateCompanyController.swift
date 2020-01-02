0262e55e87875500683b93e08e32e460cd746eec
tomo.h <tomo.the.slyb@gmail.com>
2020-01-02 20:44:25 +0900

    refactoring

--- IntermidiateTraning/CreateCompanyController.swift
+++ IntermidiateTraning/CreateCompanyController.swift
@@ -170,7 +170,6 @@ class CreateCompanyController: UIViewController, UINavigationControllerDelegate,
             company.setValue(imageData, forKey: "imageData")
         }
         
-        
         // perform the save
         do {
             try context.save()
