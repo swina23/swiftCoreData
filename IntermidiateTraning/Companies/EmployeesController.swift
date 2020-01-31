8689e304acfa796de1800331b3ba56be76de1856
tomo.h <tomo.the.slyb@gmail.com>
2020-01-31 22:34:35 +0900

    List Employees and Tuples - WIP

--- IntermidiateTraning/Companies/EmployeesController.swift
+++ IntermidiateTraning/Companies/EmployeesController.swift
@@ -7,19 +7,64 @@
 //
 
 import UIKit
+import CoreData
 
 class EmployeesController: UITableViewController {
     
     var company: Company?
     
+    var employees = [Employee]()
+    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         navigationItem.title = company?.name
+        
+        fetchEmployees()
+        
     }
+    
+    private func fetchEmployees() {
+        print("Tring to fetch employees.")
+        
+        let context = CoreDataManager.shared.persistentContainer.viewContext
+        
+        let request = NSFetchRequest<Employee>(entityName: "Employee")
+        
+        do {
+         let employees =  try context.fetch(request)
+            self.employees = employees
+//            employees.forEach{print("Employee name:", $0.name ?? "")}
+        } catch let err {
+            print("Fail to fetch employees", err)
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return employees.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath)
+        
+        let employee = employees[indexPath.row]
+        cell.textLabel?.text = employee.name
+        cell.backgroundColor = UIColor.tealColor
+        cell.textLabel?.textColor = UIColor.white
+        cell.textLabel?.font = UIFont.boldSystemFont(ofSize: 15)
+        
+        
+        
+        
+        return cell
+    }
+    
+    let cellId = "celllllllllllllId"
     
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.backgroundColor = .darkBlue
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellId)
         
         setupPlusButtonInNavBar(selector: #selector(handleAdd))
     }
